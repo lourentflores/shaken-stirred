9a540bf4abf849587cf567205ebf793843d7544a
Lourent Flores <lourent.flores@gmail.com>
2021-03-11 15:39:17 -0500

    added mongo-uri to env file

--- server/models/cocktailsModel.js
+++ server/models/cocktailsModel.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const MONGO_URI =
-  'mongodb+srv://Lourent:JehoCruz@cluster0.3hwu4.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI, {
-    // options for the connect method to parse the URI
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // sets the name of the DB that our collections are part of
     dbName: 'Cocktails',
   })
   .then(() => console.log('Connected to Mongo DB.'))
